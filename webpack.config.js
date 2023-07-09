8e211d2a61398986f97207f6873ccafea470c5a7
anhnguyen <pnguyen63@mylangara.ca>
2023-07-09 15:48:12 -0700

    Merge Login page

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
-const path =require('path')
+const path = require("path");
 
-
-module.exports ={
-    mode: 'development',
-    entry: {
-        proStart:'./src/proStart.js',
-        showServices: './src/showServices.js',
-        fetchList: './src/fetchList.js',
-        edit: './src/edit.js',
-        booking: './src/booking.js',
-        bookingConfirm: './src/bookingConfirm.js',
-        prosDashboard: './src/prosDashboard.js',
-        customerDashboard: './src/customerDashboard.js',
-        customerRegister: './src/customerRegister.js'
-    },
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    watch: true,
-    devtool: "eval-source-map"
-}
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  entry: {
+    proStart: "./src/proStart.js",
+    showServices: "./src/showServices.js",
+    fetchList: "./src/fetchList.js",
+    edit: "./src/edit.js",
+    booking: "./src/booking.js",
+    bookingConfirm: "./src/bookingConfirm.js",
+    prosDashboard: "./src/prosDashboard.js",
+    customerDashboard: "./src/customerDashboard.js",
+    customerRegister: "./src/customerRegister.js",
+    login: "./src/login.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  watch: true,
+  devtool: "eval-source-map",
+};
