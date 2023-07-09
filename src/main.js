7e433bc5ab79ae797562c0a9c97493fe1fb60103
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2023-07-09 11:01:41 -0700

    connect firebase and tomtom api
    connect firebase and tomtom api

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
-    getFirestore, collection, query, where, getDocs, setDocs
-} from 'firebase/firestore'
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    getFirestore, collection, query, where, getDocs, getDoc,setDoc, addDoc, onSnapshot, doc
+} from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD7wzxQRs4mKcMOB0Vcydzdxl0NRtZbXno",
   authDomain: "styleconnect-e781a.firebaseapp.com",
@@ -17,40 +14,133 @@ const firebaseConfig = {
   appId: "1:700825424755:web:a0fcfadde53d4248912b06",
   measurementId: "G-BW2ZJHSJ2G"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // console.log(app);
 // const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const colRefListing = collection(db, 'kimi_test')
-//
+const auth = getAuth();
 
-console.log()
-const storeAdd = document.querySelector(".add");
-storeAdd.addEventListener("submit", (e)=>{
+// to pick up specific User's address and display it.
+onAuthStateChanged(auth, async (user)=>{
+  if (user) {
+
+    // get a user's ID (dynamic).
+    const userId = user.uid;
+
+    // Refer to the specific document and their ID.
+    const userRef = doc (colRefListing, userId);
+
+    async function displayAddress() {
+      try {
+        // get documents from the collection which I want to access.
+    
+        const userDocSnap = await getDoc(userRef);
+        const accessAddress = userDocSnap.data().address;
+
+        addressContainer.innerHTML = accessAddress;
+    
+        // go through every document in the collection
+        // querySnap.forEach((document)=> {
+        //   // get each address /  .data() method refer to the data which firebase has.
+        //   console.log(document.data().address);
+        //   document.data()
+        // })
+      }
+      catch(error){
+        console.error("error", error);
+      }
+    }
+    await displayAddress();
+  } else {
+
+  }
+} )
+
+const displayBtn = document.querySelector("#gbtn");
+displayBtn.addEventListener("click", (e)=>{
+e.preventDefault();
+// console.log(23)
+displayAddress();
+});
+
+const addressContainer = document.getElementById('addressContainer');
+
+
+const storeBtn = document.querySelector(".add");
+storeBtn.addEventListener("submit", (e)=>{
 e.preventDefault();
 addAddress();
-
 });
 // console.log(colRefListing)
 // console.log("ssss")
 
 async function addAddress() {
-  console.log("addAddress called")
+  console.log("addAddress called");
+  try {
+    const address = document.getElementById("address").value;
+    // console.log(address);
+    const docRef = await setDoc(colRefListing, {
+      address});
+    // console.log("Data added to Firebase successfully");
+  } catch (error) {
+    // console.error("Error adding data to Firestore", error);
+  };
 }
 
+// const displayBtn = document.querySelector("#gbtn");
+// displayBtn.addEventListener("click", (e)=>{
+// e.preventDefault();
+// // console.log(23)
+// displayAddress();
+// });
 
+// const addressContainer = document.getElementById('addressContainer');
 
-// todoForm.addEventListener('submit', addTodo); //benefits from validation
-
-// async function addTodo() {
-//   console.log('addTodo called');
-//   let todoName = document.getElementById('name').value;
-//   let dueValue = document.getElementById('due').value;
-//   // use firestore API to add a document to DB_NAME collection //
+// //
+// async function displayAddress() {
 //   try {
-//     const docRef = await addDoc( collection( db , DB_NAME ) , { todoName , dueValue} );
-//     console.log(docRef);
-//   } catch(error){
-//     console.log(error);
+//     // get documents from the collection which I want to access.
+
+//     const querySnap = await getDoc(docRef);
+//     addressContainer.innerHTML = querySnap.data().address;
+
+//     // go through every document in the collection
+//     // querySnap.forEach((document)=> {
+//     //   // get each address /  .data() method refer to the data which firebase has.
+//     //   console.log(document.data().address);
+//     //   document.data()
+//     // })
 //   }
+//   catch(error){
+//     console.error("error", error);
+//   }
+// }
+
+
+
+  //   if (docSnap.exists()) {
+  //     const data = docSnap.data();
+
+  //     // ドキュメントのアドレスを表示する要素を作成し、テキストを設定する
+  //     const addressElement = document.createElement('div');
+  //     addressElement.textContent = `Address: ${data.address}`;
+
+  //     // アドレスを表示する要素をコンテナに追加する
+  //     addressContainer.appendChild(addressElement);
+  //   } else {
+  //     console.log('Document does not exist');
+  //   }
+  // } catch (error) {
+  //   console.error('Error displaying address:', error);
+  // }
+
+// 特定のドキュメントのアドレスを表示する（例: ドキュメントIDが"exampleDocumentId"の場合）
+// displayAddress("exampleDocumentId");
+
+
+// onSnapshot(colRefListing,(querySnapshot)=>{
+//   const total = query
+// })
